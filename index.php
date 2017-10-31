ea841f2455e230477c51f3593f06e8f85de0cfcf
Christopher McLaughlin <chris@pingalamedia.co.uk>
2017-10-31 12:15:19 +0000

    added colour to console

--- index.php
+++ index.php
@@ -137,4 +137,6 @@ $nouns = [
 $rand_key1 = array_rand($adjs, 1);
 $rand_key2 = array_rand($nouns, 1);
 
-echo $adjs[$rand_key1].$nouns[$rand_key2];
+$name = $adjs[$rand_key1].$nouns[$rand_key2];
+
+echo "\033[32m $name \033[0m\n";
